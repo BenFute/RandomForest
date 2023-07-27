99e4302e8a076c1102e221ea0b3842d3c15069f9
Ben Fute <https://benfute@github.com>
2023-07-27 17:40:34 -0500

    Data set structure and Decision Tree structure

--- RandomForest/main.c
+++ RandomForest/main.c
@@ -1,11 +1,28 @@
-//
-//  main.c
-//  RandomForest
-//
-//  Created by Ben Fute on 7/27/23.
-//
+
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define NUM_TREES 100
+#define MAX_DEPTH 10
+
+// Structure to hold the dataset
+typedef struct {
+    double features[2]; // Assume we have two features for simplicity
+    int label;  // Binary classification label (0 or 1)
+} DataPoint;
+
+// Decision tree node
+typedef struct Node {
+    int feature_index; //Index of the feature to split on
+    double threshold; // Value to split the feature on
+    int left_label; // Label if left child is a leaf node
+    int right_label; //Label if right child is a leaf node
+    struct Node* left;
+    struct Node* right;
+} Node;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
