1ae2347a65901581dacdf629ccaa9ee498bbcdad
Ben Fute <https://benfute@github.com>
2023-07-27 17:50:27 -0500

    Calculate_gini_impurity function

--- RandomForest/main.c
+++ RandomForest/main.c
@@ -1,4 +1,4 @@
-
+//A simplified decision tree that can handle binary classificiation tasks.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +23,27 @@ typedef struct Node {
     struct Node* left;
     struct Node* right;
 } Node;
+
+//Gini impurity is one the measures used to evaluate how "impure" a node is in a decision tree.  In the context of Random Forests, it is used to access the quality of a split during the tree-building process.
+//This funciton is used during hte process of building the deciision trees in the Random Forest to find the best feature and threshold to split the data at each node.
+//The goal is to find the split that minimizes the Gini impurity in the resulting child nodes, thereby improving the overall quality of the decision tree and the Random Forest
+double calculate_gini_impurity(int* class_counts, int num_classes) {
+    double impurity = 1.0;
+    int total_samples = 0;
+    
+    //Sum of class_counts
+    for (int i = 0; i < num_classes; i++) {
+        total_samples += class_counts[i];
+    }
+    
+    for (int i = 0; i < num_classes; i++) {
+        double p_i = (double)class_counts[i] / total_samples; // Compute the probability of an element belonging to class `i`
+        impurity -= p_i * p_i;  //subtract the square of the probability from the impurity.
+    }
+    
+    return impurity;
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
